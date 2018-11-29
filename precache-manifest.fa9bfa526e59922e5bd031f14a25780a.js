d8ff7d339ffd145d2af8a78f51bd201cb383cb29
David <dph0718@gmail.com>
2018-11-28 20:00:44 -0700

    Updates

---
+++ precache-manifest.fa9bfa526e59922e5bd031f14a25780a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5098b2c3d1417da8ccd9764612248ca",
+    "url": "/testsite/static/media/tileable_wood_texture.d5098b2c.png"
+  },
+  {
+    "revision": "e43410103330cabaa9874145635b9c28",
+    "url": "/testsite/static/media/navplank.e4341010.png"
+  },
+  {
+    "revision": "bfe900b1433afc608a54",
+    "url": "/testsite/static/js/runtime~main.bfe900b1.js"
+  },
+  {
+    "revision": "69ba6e29ccf17cc5b5c9",
+    "url": "/testsite/static/js/main.69ba6e29.chunk.js"
+  },
+  {
+    "revision": "c2aeeb155513c7f2e43e",
+    "url": "/testsite/static/js/1.c2aeeb15.chunk.js"
+  },
+  {
+    "revision": "69ba6e29ccf17cc5b5c9",
+    "url": "/testsite/static/css/main.e777b745.chunk.css"
+  },
+  {
+    "revision": "34ae969e2f0210c96a386f68ac01a123",
+    "url": "/testsite/index.html"
+  }
+];
